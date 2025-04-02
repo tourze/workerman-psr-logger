401aef1db90270b64c1a20f1f7e8cdb8f6c62af3
tourze-ci <you@example.com>
2025-04-02 08:17:34 +0000

    chore(workerman-psr-logger): 添加 clue/hexdump 依赖并移动 LogUtil 文件

---
+++ src/LogUtil.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tourze\Workerman\PsrLogger;
+
+use Clue\Hexdump\Hexdump;
+use Workerman\Worker;
+
+class LogUtil
+{
+    private static ?Hexdump $dumper = null;
+
+    public static function init(): void
+    {
+        if (!self::$dumper) {
+            self::$dumper = new Hexdump();
+        }
+    }
+
+    public static function debug(string $message, ?string $binaryData = null): void
+    {
+        self::init();
+        if ($binaryData !== null) {
+            Worker::log(sprintf("[DEBUG] %s:\n%s", $message, self::$dumper->dump($binaryData)));
+        } else {
+            Worker::log(sprintf("[DEBUG] %s", $message));
+        }
+    }
+
+    public static function info(string $message, ?string $binaryData = null): void
+    {
+        self::init();
+        if ($binaryData !== null) {
+            Worker::log(sprintf("[INFO] %s:\n%s", $message, self::$dumper->dump($binaryData)));
+        } else {
+            Worker::log(sprintf("[INFO] %s", $message));
+        }
+    }
+
+    public static function error(string $message, ?\Throwable $e = null): void
+    {
+        if ($e) {
+            Worker::log(sprintf("[ERROR] %s: %s\n%s", $message, $e->getMessage(), $e->getTraceAsString()));
+        } else {
+            Worker::log(sprintf("[ERROR] %s", $message));
+        }
+    }
+
+    public static function warning(string $message, ?string $binaryData = null): void
+    {
+        self::init();
+        if ($binaryData !== null) {
+            Worker::log(sprintf("[WARNING] %s:\n%s", $message, self::$dumper->dump($binaryData)));
+        } else {
+            Worker::log(sprintf("[WARNING] %s", $message));
+        }
+    }
+}
